ade0fffe7602244b8f86f856f9a1c0aad7650cc6
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-11-29 10:34:11 -0500

    Better collision angle

--- game.js
+++ game.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-=======
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
->>>>>>> origin/gh-pages
 var key = {
 	up: 38,
 	down: 40,
 	w: 87,
 	s: 83,
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-function Ball(x, y, ctx, game) {
-=======
-var hue = 120;
-=======
->>>>>>> gh-pages
 
 //Borrowed from TheCodePlayer
 function Particle( x, y, particles ) {
@@ -86,26 +75,10 @@ function Ball(x, y, game) {
 	this.startingX = x;
 	this.startingY = y;
 	this.startingSpeed = 5;
->>>>>>> origin/gh-pages
 	this.x = x;
 	this.y = y;
 	this.vx = 1;
 	this.vy = 1;
-<<<<<<< HEAD
-	this.ctx = ctx;
-	this.game = game;
-
-	this.render = function() {
-		this.ctx.beginPath();
-		this.ctx.arc(this.x, this.y, 5,0,2*Math.PI);
-		this.ctx.closePath();
-		this.ctx.fill();
-	}.bind(this);
-
-	this.update = function() {
-		this.x += this.vx;
-		this.y += this.vy;	
-=======
 	this.speed = this.startingSpeed;
 	this.game = game;
 	this.width = 15;
@@ -123,7 +96,7 @@ function Ball(x, y, game) {
 
 	Ball.prototype.reverse = function() {
 		this.vx = -this.vx;
-		this.vy = (this.vy + getRandomInt(0,1) );
+		this.vy = (this.vy + getRandomInt(0,2) );
 		this.speed += 0.5;	
 	};
 
@@ -199,7 +172,6 @@ function Ball(x, y, game) {
 
 		this.x += this.vx * this.speed;
 		this.y += this.vy * this.speed;	
->>>>>>> origin/gh-pages
 
 		if (this.x > this.game.width) {
 			this.vx = -1;
@@ -209,59 +181,18 @@ function Ball(x, y, game) {
 			this.vy = -this.vy;
 		}
 
-<<<<<<< HEAD
-		if (this.x == 0) {
-			this.vx = 1;
-		}
-
-		if (this.y == 0) {
-=======
 		if (this.x <= 0) {
 			this.vx = 1;
 		}
 
 		if (this.y <= 0) {
-<<<<<<< HEAD
->>>>>>> origin/gh-pages
-			this.vy = 1;
-=======
 			this.vy = -this.vy;
->>>>>>> gh-pages
 		}
 
 	};
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-function Paddle(x, y, ctx) {
-
-	this.ctx = ctx;
-	this.x = x;
-	this.y = y;
-
-	this.render = function() {
-		this.ctx.fillStyle = "green";
-		this.ctx.fillRect(this.x, this.y, 5, 40);
-	}.bind(this);
-
-	this.update = function(keys) {
-		if (keys[key.up] || keys[key.w]) {
-			this.y--;
-		}
-
-		if (keys[key.down] || keys[key.s]) {
-			this.y++;
-		}
-	}.bind(this);
-
-};
-
-=======
-function Paddle(x, y, ctx, aiControlled, game) {
-=======
 function Paddle(x, y, aiControlled, game, ball, keys) {
->>>>>>> gh-pages
 
 	this.game = game;
 
@@ -310,15 +241,10 @@ function SeparationLine(canvas) {
 
 	this.canvas = canvas;
 
-<<<<<<< HEAD
->>>>>>> origin/gh-pages
-function Pong() {
-=======
 	SeparationLine.prototype.render = function(context) {
 		context.fillStyle = "rgb(0, 100, 100)";
 		context.fillRect(this.canvas.width/2, 0, 2, this.canvas.height);
 	};
->>>>>>> gh-pages
 
 	SeparationLine.prototype.update = function() {
 		//Nothing to update
@@ -328,13 +254,6 @@ function Pong() {
 function Score(game) {
 	this.game = game;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	this.start = function() {
-=======
-	this.particles = [];
-	this.createParticles = function(x, y ) {
-=======
 	Score.prototype.render = function(context) {
 		context.font='30px "Lucida Console", Monaco, monospace';
 		var scoreX = (this.game.width / 2) - 30;
@@ -348,7 +267,6 @@ function Score(game) {
 	};
 
 	Score.prototype.update = function() {
->>>>>>> gh-pages
 		
 	}
 }
@@ -363,12 +281,6 @@ function Particles() {
 		}
 	};
 
-<<<<<<< HEAD
-	this.start = function() {
-		var background = new Audio("Hot_Heat.mp3"); // buffers automatically when created
-		background.play();
->>>>>>> origin/gh-pages
-=======
 	Particles.prototype.render = function(context) {
 		var i = this.particles.length;
 		while( i-- ) {
@@ -406,7 +318,6 @@ function Game() {
 		}
 		
 		var game = this;
->>>>>>> gh-pages
 
 		window.addEventListener("keydown", function (e) {
   			game.keys[e.keyCode] = true;
@@ -430,129 +341,14 @@ function Game() {
 		this.entities.push(this.particles);
 		this.entities.push(this.score);
 		
-<<<<<<< HEAD
-<<<<<<< HEAD
-		var leftBar = new Paddle(10, 10, this.ctx)
-		var rightBar = new Paddle(this.canvas.width - 20, 10, this.ctx);
-=======
-		var leftBar = new Paddle(30, 10, this.ctx, false, game)
-		var rightBar = new Paddle(this.canvas.width - 50, 10, this.ctx, true, game);
->>>>>>> origin/gh-pages
-
-		var ball = new Ball(game.width / 2, game.height / 2, this.ctx, game);
 		
-
-<<<<<<< HEAD
-
-		(function animloop(time){
-			requestAnimationFrame(animloop);
-			game.clearCanvas();
-
-			  game.ctx.fillRect(game.canvas.width/2, 0, 2, game.canvas.height);
-
-			leftBar.update(game.keys);
-			ball.update();	
-			leftBar.render();
-			rightBar.render();	
-			ball.render();
-=======
-=======
-		
->>>>>>> gh-pages
 		var frame = 1;
 		(function animloop(time){
 			frame++;
 			requestAnimationFrame(animloop);
 			
-<<<<<<< HEAD
-			
-
-			leftBar.update(game.keys, ball);
-			rightBar.update(game.keys, ball);
-			ball.update();	
-
-			if (ball.vx > 0) {
-			
-        		if (rightBar.x <= ball.x + ball.width / 2 && rightBar.x > ball.x - (ball.vx * ball.speed) + ball.width / 2) {
-        			
-            		var collisionDiff = ball.x + ball.width / 2 - rightBar.x;
-            		var k = collisionDiff/(ball.vx * ball.speed);
-            		var y = (ball.vy * ball.speed )*k + (ball.y - (ball.vy* ball.speed));
-            		if (y >= rightBar.y && y + ball.height <= rightBar.y + rightBar.height) {
-            			ball.reverse();
-            			ball.x = rightBar.x - ball.width;
-		                ball.y = Math.floor(ball.y - (ball.vy * ball.speed) + (ball.vy * ball.speed)*k);
-
-		                game.createParticles(ball.x, ball.y)
-
-		                var snd = new Audio("scifi002.wav"); // buffers automatically when created
-						snd.play();
-
-            		}
-        		}
-    		} else {
-        		if (leftBar.x + leftBar.width >= ball.x) {
-            		var collisionDiff = leftBar.x + leftBar.width / 2 - ball.x;
-            		var k = collisionDiff/-(ball.vx * ball.speed);
-            		var y = (ball.vy * ball.speed) * k + (ball.y - (ball.vy * ball.speed));
-            		if (y >= leftBar.y && y + ball.height <= leftBar.y + leftBar.height) {
-                		ball.reverse();
-                		ball.x = leftBar.x + leftBar.width;
-                		ball.y = Math.floor(ball.y - (ball.vy * ball.speed) + (ball.vy * ball.speed)*k);
-
-                		game.createParticles(ball.x, ball.y);
-                		   var snd = new Audio("scifi002.wav"); // buffers automatically when created
-						snd.play();
-            		}
-        		}
-    		}
-
-			var i = game.particles.length;
-			while( i-- ) {
-				game.particles[i].render(game.ctx);
-				game.particles[i].update(i);
-			}
-
-			if (ball.x <= 0) {
-				rightBar.score++;
-				ball.reset();
-			}
-
-			if (ball.x >= game.width) {
-				leftBar.score++;
-				ball.reset();
-			}
-
-			game.ctx.globalCompositeOperation = "source-over";
-			//game.clearCanvas();
-			game.ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-			game.ctx.fillRect(0, 0, game.width, game.height);
-			
-
-			game.ctx.fillStyle = "rgb(0, 100, 100)";
-			game.ctx.fillRect(game.canvas.width/2, 0, 2, game.canvas.height);
-			
-			leftBar.render();
-			rightBar.render();	
-
-			game.ctx.globalCompositeOperation = "lighter";
-			ball.render();
-			
-			game.ctx.font='30px "Lucida Console", Monaco, monospace';
-			var scoreX = (game.width / 2) - 30;
-			if (leftBar.score > 9) {
-				scoreX -= 10;
-			}
-
-			game.ctx.fillStyle = "rgb(0, 100, 100)";
-			game.ctx.fillText(leftBar.score, scoreX, 40);
-			game.ctx.fillText(rightBar.score, (game.width / 2) + 15, 40);
-
->>>>>>> origin/gh-pages
-=======
 			game.update();
 			game.render();
->>>>>>> gh-pages
 		})();
 		
 	};
